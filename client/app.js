0cf879c6ff7abfe26f2e0f44093ce8be3c0fb1ea
Paul Bastowski <pbastowski@gmail.com>
2016-01-03 18:36:27 +0100

    Updated anguar-babel to v1.0.9, which fixes issues with the check package. Added pbastowski:systemjs to test if this version of angular2-now still works with systemjs. Removed @View decorator and moved template to @Component, because that's the new way to do it. Added System.import('client/index)' to index.html. The first import must be done with System.import. Added index.js, which imprts app.js and kicks off the app. Added babel.json, which intructs Babel to use the SystemJS module format.

--- client/app.js
+++ client/app.js
@@ -1,25 +1,20 @@
-angular.element(document).ready(()=> {
-    angular2now.init();
-    let { SetModule, Component, View, Inject, bootstrap, options } = angular2now;
+let { SetModule, Component, View, Inject, bootstrap, options } = angular2now;
 
-    options({controllerAs: 'vm'});
+options({controllerAs: 'vm'});
 
-    SetModule('app', ['angular-meteor']);
+SetModule('app', ['angular-meteor']);
 
-    @Component({
-        selector: 'app',
-    })
-    @View({
-        template: `<h2>Welcome to angular2-now!</h2>`
-    })
-    @Inject('$http')
-    class App {
-        constructor($http) {
-            console.log('$http:', $http);
-        }
-    }
-
-    bootstrap(App);
-
-    console.log('! app');
+@Component({
+    selector: 'app',
+    template: `<h2>Welcome to angular2-now human!</h2>`
 })
+@Inject('$http')
+class App {
+    constructor($http) {
+        console.log('$http:', $http);
+    }
+}
+
+bootstrap(App);
+
+console.log('! app');
