85f248e80a9ec4cba87b3d06ddad819b13c72c14
Paul Bastowski <pbastowski@gmail.com>
2016-01-03 13:26:23 +0100

    Added SystemJS self registration.

--- packages/ng2now/ng2now.js
+++ packages/ng2now/ng2now.js
@@ -1,2 +1,17 @@
 // Write your package code here!
-angular2now.init();
+if (typeof System !== 'undefined' && System.register) {
+  System.register('angular2now', [], function(_export) {
+    for (var i in angular2now) {
+      _export(i, angular2now[i]);
+    }
+
+    return {
+      setters: [],
+      execute: function() {
+        angular2now.init();
+      }
+    };
+  });
+} else {
+  angular2now.init();
+}
